887f3c734925dfba827194bbd4a49962a91e81ff
KITCOOP <KITCOOP@DESKTOP-J354ADO>
2023-02-10 10:04:22 +0900

    14일차(자바시험)

--- day01/src/helloworld/Hello.java
+++ day01/src/helloworld/Hello.java
@@ -4,6 +4,13 @@ public class Hello {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!!!!");
+		System.out.println("1" + "2"); 
+		System.out.println(true); 
+		System.out.println(true + ""); 
+		System.out.println('J' + "ava");
+		System.out.println("" + null);
+//		System.out.println(true + null);
+
 	}
 
 }
